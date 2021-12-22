8bc69f505b66b1d5d8aa7b09e65debbba8caa083
azula9713 <nnviduranga@gmail.com>
2021-12-22 12:32:02 +0530

    Added Hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import styled from "styled-components";
+interface styledProps {
+  show: boolean;
+}
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <HeaderContainer>
       <a>
@@ -18,7 +24,11 @@ function Header() {
       <MenuAction>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu>
+        <CustomMenu
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -34,6 +44,58 @@ function Header() {
           </svg>
         </CustomMenu>
       </MenuAction>
+      <BurgerNav show={isOpen}>
+        <CloseWrapper>
+          <CustomClose
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </CustomClose>
+        </CloseWrapper>
+
+        <li>
+          <a href="#">Model S</a>
+        </li>
+        <li>
+          <a href="#">Model 3</a>
+        </li>
+        <li>
+          <a href="#">Model X</a>
+        </li>
+        <li>
+          <a href="#">Model Y</a>
+        </li>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-In</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+      </BurgerNav>
     </HeaderContainer>
   );
 }
@@ -50,6 +112,7 @@ const HeaderContainer = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 
 const MenuContainer = styled.div`
@@ -86,4 +149,37 @@ const CustomMenu = styled.button`
   border: none;
   width: 25px;
   cursor: pointer;
+`;
+
+const BurgerNav = styled.div<styledProps>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s;
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: bold;
+    }
+  }
+`;
+
+const CustomClose = styled(CustomMenu)``;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
